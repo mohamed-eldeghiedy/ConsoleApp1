cbaf8a86acdaf1e595901c3bb1c6f0deefad9d64
mohamed-eldeghiedy <eldegedym@gmail.com>
2025-07-11 21:01:47 +0300

    q2

--- ConsoleApp1/Class1.cs
+++ ConsoleApp1/Class1.cs
@@ -3,20 +3,51 @@
     public class Calculator()
     {
 
-        public  int Add(int a, int b)
+        public int Add(int a, int b)
         {
             return a + b;
         }
 
 
-        public  int Add(int a, int b, int c)
+        public int Add(int a, int b, int c)
         {
             return a + b + c;
         }
 
-        public  double Add(double a, double b)
+        public double Add(double a, double b)
         {
             return a + b;
         }
     }
+
+
+    public class Rectangle
+    {
+        public int Length { get; set; }
+        public int Width { get; set; }
+
+
+        public Rectangle()
+        {
+            Length = 0;
+            Width = 0;
+        }
+        public Rectangle(int length, int width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        public Rectangle(int size)
+        {
+            Width = size;
+            Length = size;
+        }
+        
+        public override string ToString()
+        {
+            return $"Rectangle(Width={Width}, Height={Length})";
+        }
+    }
+
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -17,6 +17,23 @@ namespace ConsoleApp1
             //Console.WriteLine(calc.Add(5.5, 4.0));
 
             #endregion
+
+
+            #region q2:
+
+            //Rectangle rectangle = new Rectangle();
+
+            //Rectangle rectangle1 = new Rectangle(5, 10);
+
+            //Rectangle rectangle2 = new Rectangle(7);
+
+            //Console.WriteLine(rectangle);
+            //Console.WriteLine(rectangle1);
+            //Console.WriteLine(rectangle2);
+
+
+
+            #endregion
         }
     }
 }
