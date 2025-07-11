c68fe0dea6ee6f984d2c0510ed866bc446f3712c
mohamed-eldeghiedy <eldegedym@gmail.com>
2025-07-11 21:51:54 +0300

    q4

---
+++ ConsoleApp1/Class2.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace Assignment06_oop
+{
+    public class Employee
+    {
+        public virtual void Work()
+        {
+            Console.WriteLine("Employee is working.");
+        }
+
+    }
+
+    public class Manager : Employee
+    {
+        public override void Work()
+        {
+            Console.WriteLine("Manager is managing .");
+            base.Work();
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -37,22 +37,31 @@ namespace ConsoleApp1
             #endregion
 
 
-
-
             #region q3: Define a class Complex Number that represents a complex number with real and imaginary parts. *Note: Overload the +, - operator to add and subtract two complex numbers.
 
 
-            ComplexNumber c1 = new ComplexNumber(8, 4);
-            ComplexNumber c2 = new ComplexNumber(3, -2);
+            //ComplexNumber c1 = new ComplexNumber(8, 4);
+            //ComplexNumber c2 = new ComplexNumber(3, -2);
 
-            ComplexNumber sum = c1 + c2;
-            ComplexNumber diff= c1 - c2;
+            //ComplexNumber sum = c1 + c2;
+            //ComplexNumber diff= c1 - c2;
 
-            Console.WriteLine($"c1 : {c1}");
-            Console.WriteLine($"c2 : {c2}");
-            Console.WriteLine($"Sum: {sum}");
-            Console.WriteLine($"Difference: {diff}");
+            //Console.WriteLine($"c1 : {c1}");
+            //Console.WriteLine($"c2 : {c2}");
+            //Console.WriteLine($"Sum: {sum}");
+            //Console.WriteLine($"Difference: {diff}");
+            #endregion
+
+
+            #region q4 : a) Create a base class named Employee with method That Work as it prints    "Employee is  working". b) Create a derived class named Manager that overrides the Work method to print "Manager is managing".
+
+            //Employee employee = new Employee();
+            //employee.Work();
+
+            //Manager manager = new Manager();
+            //manager.Work();
+
             #endregion
         }
-    }
+}
 }
